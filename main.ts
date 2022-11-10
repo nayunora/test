2b80bb2b641c695e7d862e5ff955be44ef5087c7
nayunora <117896579+nayunora@users.noreply.github.com>
2022-11-10 16:47:23 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onGesture(Gesture.TiltRight, function () {
+    music.playMelody("C5 G B A F A C5 B ", 120)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.slide), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    200,
+    600,
+    255,
+    0,
+    150,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . # .
+        # . . . #
+        . . . . #
+        # . . . #
+        . . . # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # .
+        `)
+})
+basic.forever(function () {
+	
+})
